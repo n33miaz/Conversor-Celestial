80a428ffc4f618b94c979c35b3ecf37ce19530c4
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2023-11-16 22:18:35 -0300

    add: Primeira Postagem do Projeto (beta)

---
+++ src/script.js
@@ -0,0 +1,32 @@
+/*
+
+// conversor de moedas
+
+const quantiaMoeda = 30;
+const cotacaoLibra = 6.02;
+const cotacaoBitcoin = 178163.4;
+
+var resultadoConversao = quantiaMoeda * cotacaoLibra; // para ver o valor em bitcoin substituir a variável "cotacaoLibra" por "cotacaoBitcoin"
+
+alert(
+  "R$: " +
+    quantiaMoeda +
+    " Libra(s) convertido em Reais são R$: " +
+    resultadoConversao.toFixed(2)
+); // alterar "Libra(s)" por "Bitcoin(s)"
+
+// conversor interestelar
+
+const sagittariusA = 26.67; // distância em anos luz do buraco negro até a terra
+const velocidadeDaLuz = 299792458; // metro por segundo
+const segundosPorAno = 365.25 * 24 * 60 * 60; // calculo = um ano em dias (considerando anos bissextos) * horas * minutos * segundos
+const conversaoTempo = sagittariusA * velocidadeDaLuz * segundosPorAno;
+const conversaoParaKm = conversaoTempo / 1000;
+
+alert(
+  "A distância em quilômetros da Terra até o buraco negro Sagittarius A* é de: " +
+    conversaoParaKm.toFixed(6) +
+    " km"
+);
+
+*/
